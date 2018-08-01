02a9bcf70abaa0c4c73cf110672b468e007899c4
Grayfield <grayfieldmm@outlook.com>
2018-08-01 18:27:30 +0700

    Register and Login

---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: { args: [3], msg: `Name length have to be at least 3 characters` }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: { msg: `Email is already in use` },
+      validate: {
+        isEmail: { msg: `Email is not valid` }
+      }
+    },
+    password: DataTypes.STRING,
+
+    passwordRaw: {
+      type: DataTypes.VIRTUAL,
+      validate: {
+        is: { 
+          args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, 
+          msg: `Password must be at least 8 characters or longer, contains at least 1 lowercase letter, 1 uppercase letter, and 1 number.`
+        }
+      }
+    },
+
+    isAdmin: DataTypes.BOOLEAN,
+  }, {});
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
--- routes/venue.js
+++
@@ -1,7 +0,0 @@
-const venue = require('express').Router()
-
-const controller = require('../controllers/venues')
-
-venue.get('/', controller.all)
-
-module.exports = venue
