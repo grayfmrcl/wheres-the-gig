242247d5f1fd94227f1048fdeebe6288f903784d
Grayfield <grayfieldmm@outlook.com>
2018-08-03 02:22:40 +0700

    artists beautify

--- controllers/artists/remove.js
+++ controllers/artists/remove.js
@@ -2,24 +2,24 @@ const models = require('../../models')
 const Artist = models.Artist
 
 const get = (req, res) => {
-    
-    Artist.findById(req.params.id)    
-    .then(artist => {
-        res.render('artists/remove.ejs',{artist})
-    })
-    .catch(err => res.send(err))        
+
+    Artist.findById(req.params.id)
+        .then(artist => {
+            res.render('artists/remove', { artist })
+        })
+        .catch(err => res.send(err))
 }
 
 const post = (req, res) => {
-    
-    Artist.findById(req.params.id)    
-    .then(artist => {
-        artist.destroy()
-        .then( () => res.redirect('/artists'))
-        .catch(err => res.send(err))        
-    })
-    .catch(err => res.send(err))     
-    
+
+    Artist.findById(req.params.id)
+        .then(artist => {
+            artist.destroy()
+                .then(() => res.redirect('/artists'))
+                .catch(err => res.send(err))
+        })
+        .catch(err => res.send(err))
+
 }
 
 module.exports = { get, post }
